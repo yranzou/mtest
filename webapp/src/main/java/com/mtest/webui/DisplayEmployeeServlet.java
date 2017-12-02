a9c431c194b27d2d8e2284a8e4e51de8a3e4fad9
yranzou <yranzou@gmail.com>
2017-12-02 21:15:47 +0300

    addad model dep

--- webapp/src/main/java/com/mtest/webui/DisplayEmployeeServlet.java
+++ webapp/src/main/java/com/mtest/webui/DisplayEmployeeServlet.java
@@ -1,7 +1,28 @@
 package com.mtest.webui;
 
+import com.mtest.lesson16.EmployeeDao;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
- * Created by yuri on 30.11.17.
+ *  Created by yuri on 30.11.17.
  */
-public class DisplayEmployeeServlet extends HttpServlet {  
+public class DisplayEmployeeServlet extends HttpServlet {
+    private static long serialVersionID = 1L;
+    private EmployeeDao employeeDao;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().write("<b>Hello, this is test!</b>".getBytes());
+    }
 }
