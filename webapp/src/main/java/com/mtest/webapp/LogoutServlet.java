f01474fbe39a90d24fd03011a0af36e189ed9a6c
yranzou <yranzou@gmail.com>
2018-03-01 21:55:33 +0300

    ui

--- webapp/src/main/java/com/mtest/webapp/LogoutServlet.java
+++ webapp/src/main/java/com/mtest/webapp/LogoutServlet.java
@@ -1,11 +1,13 @@
 package com.mtest.webapp;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -18,6 +20,11 @@ public class LogoutServlet extends HttpServlet {
         session.invalidate();
         String pageToForward = request.getContextPath();
         response.sendRedirect(pageToForward);
+//        PrintWriter out = response.getWriter();
+//        out.println("In dispatcherServlet <BR>");
+//
+//        RequestDispatcher rd = request.getRequestDispatcher(pageToForward);
+//        rd.include(request, response);
 
     }
 }
